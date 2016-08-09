a3624f6992fedb6bef7d7984b0d161a8086884b8
Dmitrii Evdokimov <diev@mail.ru>
2016-08-09 19:18:15 +0300

    Create AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,39 @@
+// Copyright (c) 2016 Dmitrii Evdokimov. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// Source https://github.com/diev/HTTPSUpload
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HTTPSUpload")]
+[assembly: AssemblyDescription("Compatible program to send files to NBKI, Moscow.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AO City Invest Bank")]
+[assembly: AssemblyProduct("HTTPSUpload")]
+[assembly: AssemblyCopyright("Copyright © 2016 Dmitrii Evdokimov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8604c884-a69a-4584-a625-356636a6d641")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.60809.0")]
+[assembly: AssemblyFileVersion("1.0.60809.0")]
